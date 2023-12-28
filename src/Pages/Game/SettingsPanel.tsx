dec775b888735fdc08192e1409fed0d279467db5
Tinellie-NewPC <1044001101@qq.com>
2023-12-28 11:10:25 +0800

    [fix]: Fix the Issue Related to Piece Click Effect (close #15)
    1. Fix the Issue Related to Piece Click Effect
    2. Improve SettingsPanel.tsx
    3. Add SettingsPanel.css & PieceJSX.css
    
    4. Modified README
    5. Add LICENSE

--- src/Pages/Game/SettingsPanel.tsx
+++ src/Pages/Game/SettingsPanel.tsx
@@ -1,16 +1,21 @@
 import React from "react";
 import {Game} from "../../Game/Game";
 
+import "./SettingsPanel.css"
+
 export function SettingsPanel({game} : {game: Game}){
     let settings = game.settings;
-    return <div>
-        <input type="checkbox" onInput={
-            (event: React.ChangeEvent<HTMLInputElement>) => {
-                settings.dontSwitchPlayer = event.target.checked;
-                console.warn(`checked: ${settings.dontSwitchPlayer}`)
-            }
-        }/>
-        <button className="sans-font" onClick={() =>{
+    return <div id="settings-panel">
+        <div id="settings-dont-switch-player" className="sans-font">
+            Dont Switch Player:
+            <input type="checkbox" onInput={
+                (event: React.ChangeEvent<HTMLInputElement>) => {
+                    settings.dontSwitchPlayer = event.target.checked;
+                    console.warn(`checked: ${settings.dontSwitchPlayer}`)
+                }
+            }/>
+        </div>
+        <button id="settings-rerender-all-button" className="sans-font" onClick={() =>{
             game.renderManager.rerenderAll();
         }}>ForceUpdate</button>
     </div>
